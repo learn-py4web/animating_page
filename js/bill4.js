2576a9f6fa3c8c1f132c7114072732fb9f3b9093
Luca de Alfaro <luca@dealfaro.com>
2024-10-02 15:17:23 -0700

    No buttons yet

---
+++ js/bill4.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+app.data = {    
+    data: function() {
+        return {
+            // Complete as you see fit.
+            entree: 0,
+            dessert: 0,
+            tip: 0.20,
+            total: 0,
+        };
+    },
+    methods: {
+        // Complete as you see fit.
+        update: function() {
+            // This is an example.
+            this.total = this.entree + this.dessert;
+        },
+    }
+};
+
+app.vue = Vue.createApp(app.data).mount("#app");
