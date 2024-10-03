393bb53dc96dd5583e1ebe635c037b7361448166
Luca de Alfaro <luca@dealfaro.com>
2024-10-02 18:34:58 -0700

    Improvements

--- js/bill3.js
+++ js/bill3.js
@@ -1,7 +1,19 @@
-var entree_el = $('input[name="entree"]');
+var entree_el = $('input[name="entree"]'); // This is actually the list of nodes that are selected.
 var dessert_el = $('input[name="dessert"]');
 var total_el = $('#total');
-var tip_amount = 0;
+var tip_amount = 0.15;
+
+function updateTotal() {
+    var entree = parseFloat(entree_el.val());
+    var dessert = parseFloat(dessert_el.val());
+    var total = entree + dessert;
+    total = total + (total * tip_amount);
+    total_el.text("$" + total.toFixed(2));
+    }
+
+// These listen for change events. 
+entree_el.change(updateTotal);
+dessert_el.change(updateTotal);
 
 // Buttons. 
 $('#tip-15').click(function() {
@@ -17,16 +29,6 @@ $('#tip-25').click(function() {
     updateTotal();
     });
         
-function updateTotal() {
-    var entree = parseFloat(entree_el.val());
-    var dessert = parseFloat(dessert_el.val());
-    var total = entree + dessert;
-    total = total + (total * tip_amount);
-    total_el.text("$" + total.toFixed(2));
-    }
 
-// These listen for change events. 
-entree_el.change(updateTotal);
-dessert_el.change(updateTotal);
 
 
